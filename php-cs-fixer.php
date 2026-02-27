bffc1bfea8c4c4d5811aeab10c74759b32a48c0f
Esteban De La Fuente Rubio <esteban@delaf.cl>
2026-02-27 16:02:43 -0300

    Se cambia a PHP 8.5. Parche en phpstan.neon por problema con CoversClass.

--- php-cs-fixer.php
+++ php-cs-fixer.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * Derafu: Foundation - Base for Derafu's Projects.
+ * Derafu: Backbone - The Architectural Spine for PHP Libraries.
  *
  * Copyright (c) 2025 Esteban De La Fuente Rubio / Derafu <https://www.derafu.dev>
  * Licensed under the MIT License.
